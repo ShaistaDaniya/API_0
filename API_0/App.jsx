51628a6e31575ffcd91812143cbc355378b72ed2
ShaistaDaniya <shaista@gigandtake.com>
2023-07-19 23:57:00 +0530

    #2

---
+++ API_0/App.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View, FlatList } from 'react-native';
+const App =() => {
+    const [data,setData]=useState([])
+     const getAPIData =async () => {
+        const url = "http://10.0.2.2:3000/user";
+        let result =await fetch(url);
+        result = await result.json();
+        setData(result);
+     }
+     useEffect(() => {
+        getAPIData()
+     }, [])
+
+  return (
+    <View>
+        <Text style={{ fontSize:30 }}> USING JSON SERVER API</Text>
+        {
+            data.lenght?
+            data.map((item)=> <View>
+            <Text style={{ fontSize:30 }}> {item.name}</Text>
+            </View>)
+            :null
+        }
+        </View>
+  )  
+}
+export default App;
